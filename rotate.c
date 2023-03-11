8c5783f8fea71fbaa661cadb660f82c20e293787
Ribas13 <diogo10ribeiro.dr@gmail.com>
2023-03-11 15:30:31 +0000

    update

--- rotate.c
+++ rotate.c
@@ -6,7 +6,7 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 15:04:57 by diosanto          #+#    #+#             */
-/*   Updated: 2023/03/01 17:02:21 by diosanto         ###   ########.fr       */
+/*   Updated: 2023/03/11 15:25:58 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 
 //ra - rotate a - Shift all elements of a by 1 - first becomes last
-/*void	ft_ra(t_node **stack_a)
+void	ft_ra(t_node **stack_a)
 {
+	
 }
 
 //rb - rotate b - Shift all elements of a by 1 - first becomes last
@@ -26,4 +27,6 @@ void	ft_rb(t_node **stack_b)
 //rr - rotate a and b at the same time
 void	ft_rr(t_node **stack_a, t_node **stack_b)
 {
-}*/
+	ft_ra(stack_a);
+	ft_rb(stack_b);
+}
