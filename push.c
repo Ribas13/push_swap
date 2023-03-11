e5fe86683a69bdfb84755f7e4bf910b206fb0c4c
Ribas13 <diogo10ribeiro.dr@gmail.com>
2023-03-11 15:51:41 +0000

    swaps done

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 15:02:43 by diosanto          #+#    #+#             */
-/*   Updated: 2023/03/01 19:44:45 by diosanto         ###   ########.fr       */
+/*   Updated: 2023/03/11 15:35:14 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,15 @@ void	ft_pa(t_node	**stack_a, t_node	**stack_b)
 }
 
 //Take the first element from stack_b and send to top of stack_a
-/*void	ft_pb(t_node **stack_b)
+void	ft_pb(t_node **stack_a, t_node **stack_b)
 {
-}*/
+	t_node	*temp;
+
+	if (*stack_b == NULL)
+		return ;
+	temp = *stack_b;
+	*stack_a = (*stack_a)->next;
+	temp->next = *stack_a;
+	*stack_a = temp;
+	printf("pb\n");
+}
--- swaps.c
+++ swaps.c
@@ -3,40 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   swaps.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: diosanto <diosanto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 14:57:03 by diosanto          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/02/27 14:53:38 by diosanto         ###   ########.fr       */
-=======
-/*   Updated: 2023/03/01 17:02:26 by diosanto         ###   ########.fr       */
->>>>>>> bc6cb91 (update)
+/*   Updated: 2023/03/11 15:51:32 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "push_swap.h"
 
-//swap a
-/*void	ft_sa(t_node **stack_a)
+/*Swap the first 2 elements at the top of stack a.
+Do nothing if there is only one or no elements.*/
+void	ft_sa(t_node **stack_a)
 {
 	t_node	*temp;
 
 	temp = *stack_a;
-	*stack_a = *stack_b;
-	*stack_b = temp;
+	*stack_a = (*stack_a)->next;
+	temp->next = (*stack_a)->next;
+	(*stack_a)->next = temp;
+	write(1, "sa\n", 3);
 }
 
-//swap b
+/*Swap the first 2 elements at the top of stack b.
+Do nothing if there is only one or no elements.*/
 void	ft_sb(t_node **stack_b)
 {
 	t_node	*temp;
 
-	temp = *stack_a;
-	*stack_a = *stack_b;
-	*stack_b = temp;
+	temp = *stack_b;
+	*stack_b = (*stack_b)->next;
+	temp->next = (*stack_b)->next;
+	(*stack_b)->next = temp;
+	write(1, "sb\n", 3);
 }
 
-//ss - swap a and b at the same time
+/*sa and sb at the same time*/
 void	ft_ss(t_node **stack_a, t_node **stack_b)
 {
-}*/
+	t_node	*temp_a;
+	t_node	*temp_b;
+
+	temp_a = *stack_a;
+	*stack_a = (*stack_a)->next;
+	temp_a->next = (*stack_a)->next;
+	(*stack_a)->next = temp_a;
+	temp_b = *stack_b;
+	*stack_b = (*stack_b)->next;
+	temp->next = (*stack_b)->next;
+	(*stack_b)->next = temp_b;
+	write(1, "ss\n", 3);
+}
